e678634c3914b66e411be1dc613c802b5272876c
angelahack1 <angelakimichellle@hotmail.com>
2024-05-31 19:43:04 -0400

    Enabling the Mailer for A&A Spa and async callback, by angelahack1

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ const bodyParser = require('body-parser')
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
 const fs = require('fs');
 var credArray = [];
+var callbackArray = [];
+var objCallback = null;
 
 aws.config.update({region: 'us-east-1'});
 
@@ -26,7 +28,7 @@ try {
 } catch (err) {
   credArray = null;
   const currentDate = new Date();
-  console.error("ERROR at Startup (loading creds) ...AixKare Mailer App v"+"10122023_1855"+" is being stopped, at: ", currentDate, ".");
+  console.error("ERROR at Startup (loading creds) ...AixKare Mailer App v"+"31052024_1720"+" is being stopped, at: ", currentDate, ".");
   process.exit(-1);
 }
 
@@ -63,6 +65,7 @@ app.get('/*', function(req, res) {
     var bodyMail = "";
     var mailOptions = {};
     var ok = true;
+
     if(typeof email != 'undefined' && email != null ) {
       if(from.indexOf("aixkare") > -1) {
         bodyMail = 'We have received a comment!!, from: '+email+'\n'+'Commentary: '+comment+'\n';
@@ -72,93 +75,80 @@ app.get('/*', function(req, res) {
           subject: 'Comment from AixKare from AixKare-Mailer',
           text: bodyMail
         };
-    } else if(from.indexOf("artecnologia") > -1) {
-      bodyMail = 'Hemos recibido un comentario!!, de: '+email+'\n'+'Comentario: '+comment+'\n';
-      mailOptions = {
-        from: 'mailer@artecnologia.aixkare.com',
-        to: 'angela.l.m@aixkare.com',
-        subject: 'Comment from Artecnologia by Artecnologia-Mailer',
-        text: bodyMail
-      };
-    } else {
+      } else if(from.indexOf("artecnologia") > -1) {
+        bodyMail = 'Hemos recibido un comentario!!, de: '+email+'\n'+'Comentario: '+comment+'\n';
+        mailOptions = {
+          from: 'mailer@artecnologia.aixkare.com',
+          to: 'angela.l.m@aixkare.com',
+          subject: 'Comment from Artecnologia by AixKare-Mailer',
+          text: bodyMail
+        };
+      } else if(from.indexOf("ayaspa") > -1) {
+        bodyMail = 'Hemos recibido un comentario!!, de: '+email+'\n'+'Comentario: '+comment+'\n';
+        mailOptions = {
+          from: 'mailer@ayaspa.aixkare.com',
+          to: 'angela.l.m@aixkare.com',
+          subject: 'Comment from A&A Spa by AixKare-Mailer',
+          text: bodyMail
+        };
+      } else {
       console.log('WARNING from indicator not present, Mail did not tried to be sent');
       res.sendStatus(200);
       return;
     }
 
-      transporter.sendMail(mailOptions, (err, info) => {
-        if(err) {
-          console.log("Error at sendmail(get): ",err);
-          ok = false;
-        } 
-        if(info) {
-          console.log("Ok at sendmail(get): envelope: ",info.envelope);
-          console.log("Ok at sendmail(get): messageId: ",info.messageId);
-          ok = true;
-        }
-      });
+      var CallbackObject = function() {
+        this.from = mailOptions.from;
+        this.to = mailOptions.to;
+        this.subject = mailOptions.subject;
+        this.text = mailOptions.txt;
+        this.lang = lang;
 
-      if(ok === true) {
-        if(from.indexOf("artecnologia") > -1) {
-          console.log('OK Message queued from Artecnologia to be sent successfully!');
-          console.log('OK get /* call succeed, Message queued to sent successfully!: <'+email+'><'+comment+'>');
-          if(lang == 'en') {
-            res.redirect("https://www.artecnologia.aixkare.com/thanks.html?lang=en");
-          } else if(lang == 'es') {
-            res.redirect("https://www.artecnologia.aixkare.com/thanks.html?lang=es");
-          } else if(lang == 'pt') {
-            res.redirect("https://www.artecnologia.aixkare.com/thanks.html?lang=pt");
-          } else{
-            res.redirect("https://www.artecnologia.aixkare.com/thanks.html?lang=en");
+        this.callback = function(err, info) {
+          console.log("CallbackObject::callback()...");
+          if(err) {
+            console.log("Error at event called-back: ");
+            console.error(err);
+          } 
+          if(info) {
+            console.log("Info at event called-back: ");
+            console.info(info);
           }
-        } else if(from.indexOf("aixkare") > -1) {
-          console.log('OK Message queued from AixKare to be sent successfully!');
-          console.log('OK get /* call succeed, Message queued to sent successfully!: <'+email+'><'+comment+'>');
-          if(lang == 'en') {
-            res.redirect("https://www.aixkare.com/thanks.html?lang=en");
-          } else if(lang == 'es') {
-            res.redirect("https://www.aixkare.com/thanks.html?lang=es");
-          } else if(lang == 'pt') {
-            res.redirect("https://www.aixkare.com/thanks.html?lang=pt");
-          } else{
-            res.redirect("https://www.aixkare.com/thanks.html?lang=en");
-          }
-        } else {
-          console.log('ERRORMessage queued but from is incorrect...!');
-          console.log('ERROR post /* call succeed, Message queued to sent successfully!: <'+email+'><'+comment+'>');
-          res.redirect("https://www.aixkare.com/index.html");
+          console.log("...CallbackObject::callback()");
+        };
+      };
+
+      var callbackObjectInstance = new CallbackObject();
+      transporter.sendMail(mailOptions, function(err, info) { callbackObjectInstance.callback(err, info); } );
+
+      if(from.indexOf("artecnologia") > -1) {
+        console.log('GET /* On Artecnología, Message queued to sent successfully!: <'+email+'><'+comment+'>');
+        if(lang == 'en') {
+          res.redirect("https://www.artecnologia.aixkare.com/thanks.html?lang=en");
+        } else if(lang == 'es') {
+          res.redirect("https://www.artecnologia.aixkare.com/thanks.html?lang=es");
+        } else if(lang == 'pt') {
+          res.redirect("https://www.artecnologia.aixkare.com/thanks.html?lang=pt");
+        } else{
+          res.redirect("https://www.artecnologia.aixkare.com/thanks.html?lang=en");
         }
+      } else if(from.indexOf("aixkare") > -1) {
+        console.log('GET /* On AiXKare, Message queued to sent successfully!: <'+email+'><'+comment+'>');
+        if(lang == 'en') {
+          res.redirect("https://www.aixkare.com/thanks.html?lang=en");
+        } else if(lang == 'es') {
+          res.redirect("https://www.aixkare.com/thanks.html?lang=es");
+        } else if(lang == 'pt') {
+          res.redirect("https://www.aixkare.com/thanks.html?lang=pt");
+        } else{
+          res.redirect("https://www.aixkare.com/thanks.html?lang=en");
+        }
+      } else if(from.indexOf("ayaspa") > -1) {
+        console.log('GET /* On A&A Spa, Message queued to sent successfully!: <'+email+'><'+comment+'>');
+        res.redirect("https://www.ayaspa.aixkare.com/gracias.html");
       } else {
-        if(from.indexOf("artecnologia") > -1) {
-          console.log('ERROR Message from Artecnologia could not be queued to be sent!!!');
-          console.log('ERROR get /* call, could not be queued to be sent!!!: query: [ ',req.query,' ],[ ',email+'>,<'+comment+'>',' ] url: '+req.url);
-          if(lang == 'en') {
-            res.redirect("https://www.artecnologia.aixkare.com/index.html?lang=en");
-          } else if(lang == 'es') {
-            res.redirect("https://www.artecnologia.aixkare.com/index.html?lang=es");
-          } else if(lang == 'pt') {
-            res.redirect("https://www.artecnologia.aixkare.com/index.html?lang=pt");
-          } else{
-            res.redirect("https://www.artecnologia.aixkare.com/index.html?lang=en");
-          }
-        } else if(from.indexOf("aixkare") > -1) {
-          console.log('ERROR Message from AixKare could not be queued to be sent!!!');
-          console.log('ERROR get /* call, could not be queued to be sent!!!: query: [ ',req.query,' ],[ ',email+'>,<'+comment+'>',' ] url: '+req.url);
-          res.redirect("https://www.aixkare.com/index.html");
-          if(lang == 'en') {
-            res.redirect("https://www.aixkare.com/index.html?lang=en");
-          } else if(lang == 'es') {
-            res.redirect("https://www.aixkare.com/index.html?lang=es");
-          } else if(lang == 'pt') {
-            res.redirect("https://www.aixkare.com/index.html?lang=pt");
-          } else{
-            res.redirect("https://www.aixkare.com/index.html?lang=en");
-          }
-        } else {
-          console.log('ERROR Message queued but from is incorrect...!');
-          console.log('ERROR post /* call succeed, Message queued to sent successfully!: <'+email+'><'+comment+'>');
-          res.redirect("https://www.aixkare.com/thanks.html");
-        }
+        console.log('ERROR Message queued but from is incorrect...!: <'+email+'><'+comment+'>');
+        res.redirect("https://www.aixkare.com/index.html");
       }
     } else {
       console.log('WARNING Mail from Message not present in method GET, Mail did not tried to be sent');
@@ -191,7 +181,7 @@ app.post('/*', function(req, res) {
         mailOptions = {
           from: 'mailer@aixkare.com',
           to: 'angela.l.m@aixkare.com',
-          subject: 'Comment from AixKare from AixKare-Mailer',
+          subject: 'Comment from AixKare from AiXKare-Mailer',
           text: bodyMail
         };
     } else if(from.indexOf("artecnologia") > -1) {
@@ -199,7 +189,15 @@ app.post('/*', function(req, res) {
       mailOptions = {
         from: 'mailer@artecnologia.aixkare.com',
         to: 'angela.l.m@aixkare.com',
-        subject: 'Comment from Artecnologia by Artecnologia-Mailer',
+        subject: 'Comment from Artecnologia by AiXKare-Mailer',
+        text: bodyMail
+      };
+    } else if(from.indexOf("ayaspa") > -1) {
+      bodyMail = 'Hemos recibido un comentario!!, de: '+email+'\n'+'Comentario: '+comment+'\n';
+      mailOptions = {
+        from: 'mailer@ayaspa.aixkare.com',
+        to: 'angela.l.m@aixkare.com',
+        subject: 'Comment from A&A Spa by AiXKare-Mailer',
         text: bodyMail
       };
     } else {
@@ -208,22 +206,32 @@ app.post('/*', function(req, res) {
       return;
     }
 
-      transporter.sendMail(mailOptions, (err, info) => {
+    var CallbackObject = function() {
+      this.from = mailOptions.from;
+      this.to = mailOptions.to;
+      this.subject = mailOptions.subject;
+      this.text = mailOptions.txt;
+      this.lang = lang;
+
+      this.callback = function(err, info) {
+        console.log("CallbackObject::callback()...");
         if(err) {
-          console.log("Error at sendmail(post): ",err);
-          ok = false;
+          console.log("Error at event called-back: ");
+          console.error(err);
         } 
         if(info) {
-          console.log("Ok at sendmail(post): envelope: ",info.envelope);
-          console.log("Ok at sendmail(post): messageId: ",info.messageId);
-          ok = true;
+          console.log("Info at event called-back: ");
+          console.info(info);
         }
-      });
+        console.log("...CallbackObject::callback()");
+      };
+    };
 
-      if(ok === true) {
+    var callbackObjectInstance = new CallbackObject();
+    transporter.sendMail(mailOptions, function(err, info) { callbackObjectInstance.callback(err, info); } );
+
         if(from.indexOf("artecnologia") > -1) {
-          console.log('OK Message queued from Artecnologia to be sent successfully!');
-          console.log('OK post /* call succeed, Message queued to sent successfully!: <'+email+'><'+comment+'>');
+          console.log('OK POST /* Artecnologia, Message queued to sent successfully!: <'+email+'><'+comment+'>');
           if(lang == 'en') {
             res.redirect("https://www.artecnologia.aixkare.com/thanks.html?lang=en");
           } else if(lang == 'es') {
@@ -234,8 +242,7 @@ app.post('/*', function(req, res) {
             res.redirect("https://www.artecnologia.aixkare.com/thanks.html?lang=en");
           }
         } else if(from.indexOf("aixkare") > -1) {
-          console.log('OK Message queued from AixKare to be sent successfully!');
-          console.log('OK post /* call succeed, Message queued to sent successfully!: <'+email+'><'+comment+'>');
+          console.log('OK POST /* AiXKare, Message queued to sent successfully!: <'+email+'><'+comment+'>');
           if(lang == 'en') {
             res.redirect("https://www.aixkare.com/thanks.html?lang=en");
           } else if(lang == 'es') {
@@ -245,43 +252,14 @@ app.post('/*', function(req, res) {
           } else{
             res.redirect("https://www.aixkare.com/thanks.html?lang=en");
           }
-        } else {
-          console.log('ERRORMessage queued but from is incorrect...!');
-          console.log('ERROR post /* call succeed, Message queued to sent successfully!: <'+email+'><'+comment+'>');
-          res.redirect("https://www.aixkare.com/thanks.html?lang=en");
-        }
-      } else {
-        if(from.indexOf("artecnologia") > -1) {
-          console.log('ERROR Message from Artecnologia could not be queued to be sent!!!');
-          console.log('ERROR post /* call, could not be queued to be sent!!!: query: [ ',req.query,' ],[ ',email+'>,<'+comment+'>',' ] url: '+req.url);
-          if(lang == 'en') {
-            res.redirect("https://www.artecnologia.aixkare.com/index.html?lang=en");
-          } else if(lang == 'es') {
-            res.redirect("https://www.artecnologia.aixkare.com/index.html?lang=es");
-          } else if(lang == 'pt') {
-            res.redirect("https://www.artecnologia.aixkare.com/index.html?lang=pt");
-          } else{
-            res.redirect("https://www.artecnologia.aixkare.com/index.html?lang=en");
-          }
-        } else if(from.indexOf("aixkare") > -1) {
-          console.log('ERROR Message from AixKare could not be queued to be sent!!!');
-          console.log('ERROR post /* call, could not be queued to be sent!!!: query: [ ',req.query,' ],[ ',email+'>,<'+comment+'>',' ] url: '+req.url);
-          res.redirect("https://www.aixkare.com/index.html");
-          if(lang == 'en') {
-            res.redirect("https://www.aixkare.com/index.html?lang=en");
-          } else if(lang == 'es') {
-            res.redirect("https://www.aixkare.com/index.html?lang=es");
-          } else if(lang == 'pt') {
-            res.redirect("https://www.aixkare.com/index.html?lang=pt");
-          } else{
-            res.redirect("https://www.aixkare.com/index.html?lang=en");
-          }
+        } else if(from.indexOf("ayaspa") > -1) {
+          console.log('OK POST /* A&A Spa, Message queued to sent successfully!: <'+email+'><'+comment+'>');
+            res.redirect("https://www.ayaspa.aixkare.com/gracias.html");
         } else {
           console.log('ERROR Message queued but from is incorrect...!');
           console.log('ERROR post /* call succeed, Message queued to sent successfully!: <'+email+'><'+comment+'>');
           res.redirect("https://www.aixkare.com/thanks.html?lang=en");
         }
-      }
     } else {
       console.log('WARNING Mail from Message not present in method POST, Mail did not tried to be sent');
       res.sendStatus(200);
@@ -291,12 +269,12 @@ app.post('/*', function(req, res) {
 
 process.on('exit', () => {
   const currentDate = new Date();
-  console.log("...AixKare Mailer App v"+"10122023_1855"+" is being stopped, at: ", currentDate, ".");
+  console.log("...AixKare Mailer App v"+"31052024_1720"+" is being stopped, at: ", currentDate, ".");
  });
 
 app.listen(3000, function() {
     const currentDate = new Date();
-    console.log("AixKare Mailer App v"+"10122023_1855"+" started, at: ", currentDate, "...");
+    console.log("AixKare Mailer App v"+"31052024_1720"+" started, at: ", currentDate, "...");
 });
 
 module.exports = app
